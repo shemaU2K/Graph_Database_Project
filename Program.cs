e6b3c8964db63b577d0e43b1acbeacde44406798
Shema_USK <illya.shema4527482@gmail.com>
2025-11-03 20:23:48 +0200

    Added the solution and project files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Graph_database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
