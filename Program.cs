714575246750024a9c857b623ce6e3a32960b635
Shema_USK <illya.shema4527482@gmail.com>
2025-11-06 01:05:43 +0200

    Enhance Graph class with node and edge management
    Updated the Graph class to manage nodes and edges, added functionality to add nodes and edges through user input.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Node<T>
 {
     public T Value { get; set; }
-    public Node(T value)
+    public List<Edge<T>> Edges { get; set; } = new List<Edge<T>>();
+    public Node(T value1)
     {
-        Value = value;
+        Value = value1;
     }
 }
+
 class Edge<T>
 {
     public Node<T> From { get; set; }
@@ -18,10 +22,70 @@ class Edge<T>
         To = to;
     }
 }
+
 class Graph<T>
 {
-    public void AddNode(Node<T> node) 
+    private Dictionary<Tkey,TValue> nodes = new Dictionary<Tkey, TValue>;
+    private List<Edge<T>> edges = new List<Edge<T>>();
+    public void AddNode(Node<T> node)
     {
-        // Implementation here
+        nodes.Add(node);
+        Console.WriteLine($"Node with value {node.Value} added.");
+    }
+    public void AddEdge(Node<T> from, Node<T> to)
+    {
+        if (!nodes.Contains(from) || !nodes.Contains(to))
+        {
+            Console.WriteLine("Error: One or both nodes do not exist in the graph."); 
+            return;
+        }
+        
+        var edge = new Edge<T>(from, to);
+        edges.Add(edge);
+        from.Edges.Add(edge);
+        Console.WriteLine($"Edge from {from.Value} to {to.Value} added.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var graph = new Graph<int>();
+        while(true)
+        {
+            Console.WriteLine("Enter command (Add_node, Add_edge, Exit):");
+            var command = Console.ReadLine();
+            if (command == "Exit") break;
+            switch (command)
+            {
+                case "Add_node":
+                    Console.WriteLine("Enter node value:");
+                    var value = int.Parse(Console.ReadLine());
+                    var node = new Node<int>(value);
+                    graph.AddNode(node);
+                    break;
+                case "Add_edge":
+                    Console.WriteLine("Enter from node value:");
+                    var fromValue = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter to node value:");
+                    var toValue = int.Parse(Console.ReadLine());
+                    var fromNode = graph.Nodes.FirstOrDefault(n => n.Value.Equals(fromValue));
+                    var toNode = graph.Nodes.FirstOrDefault(n => n.Value.Equals(toValue));
+                    if (fromNode != null && toNode != null)
+                    {
+                        graph.AddEdge(fromNode, toNode);
+                    }
+                    else
+                    {
+                        Console.WriteLine("One or both nodes not found.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Unknown command.");
+                    break;
+            }
+        }
+     
     }
 }
