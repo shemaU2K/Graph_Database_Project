3b4fab8e6e46bf5f8d15247589d7d757b2986e47
Shema_USK <illya.shema4527482@gmail.com>
2025-12-05 03:53:40 +0200

    Load WPF project

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Graph_Database_WPF.ViewModels;
+using System.Windows;
+
+namespace Graph_Database_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
